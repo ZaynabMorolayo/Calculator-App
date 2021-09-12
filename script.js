aeb69000cf925fefe17c74c25fa75783ba0a9dff
ZaynabMorolayo <zebzy2010@gmail.com>
2021-09-12 09:12:19 +0100

    developers link

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 var resultField = $('#result');
+
 function insertNumber (number) {
     var existingNumber = resultField.val()
-    resultField.val(existingNumber + number )
+    if (existingNumber == 0) {
+        existingNumber = '';
+        resultField.val(existingNumber + number)
+    } else {
+        resultField.val(existingNumber + number)
+    }
+        
+    
 };
 function cNumber() {
-    resultField.val('')
+    resultField.val(0)
 }
 
 function calculate(){
@@ -13,8 +21,12 @@ function calculate(){
 }
 function deleteNumber (){
     var valou = resultField.val()
-
+    
     if(valou!='') {
         resultField.val(resultField.val().slice(0,-1));
+        if(valou.length - 1 == 0) {
+            resultField.val(0);
+        }
     }
+    
 }
